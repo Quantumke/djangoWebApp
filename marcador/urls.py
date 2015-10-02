d71905c268dbdc45441f308f92ebaa19de823611
Benson Mburu <nguruben@gmail.com>
2015-10-02 16:03:18 +0300

    changed urls for forms

--- marcador/urls.py
+++ marcador/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     url(r'^user/(?P<username>[-\w]+)/$', 'marcador.views.bookmark_user',
         name='marcador_bookmark_user'),
     url(r'^$', 'marcador.views.bookmark_list', name='marcador_bookmark_list'),
+     url(r'^create/$', 'marcador.views.bookmark_create',
+        name='marcador_bookmark_create'),
+    url(r'^edit/(?P<pk>\d+)/$', 'marcador.views.bookmark_edit',
+        name='marcador_bookmark_edit'),
+    url(r'^$', 'marcador.views.bookmark_list', name='marcador_bookmark_list'),
+]
 ]
