e5066cb967ff553d91ebf7204095681b7e0f1d3e
Benson Mburu <nguruben@gmail.com>
2015-10-02 12:54:57 +0300

    identitation errors resolved

--- marcador/models.py
+++ marcador/models.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
@@ -5,23 +6,25 @@ from django.utils.timezone import now
 
 
 @python_2_unicode_compatible
-
-
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
-class Meta:
+
+    class Meta:
         verbose_name = 'tag'
         verbose_name_plural = 'tags'
         ordering = ['name']
 
-def __str__(self):
+    def __str__(self):
         return self.name
+
 
 class PublicBookmarkManager(models.Manager):
     def get_queryset(self):
         qs = super(PublicBookmarkManager, self).get_queryset()
         return qs.filter(is_public=True)
 
+
+@python_2_unicode_compatible
 class Bookmark(models.Model):
     url = models.URLField()
     title = models.CharField('title', max_length=255)
@@ -33,16 +36,15 @@ class Bookmark(models.Model):
         related_name='bookmarks')
     tags = models.ManyToManyField(Tag, blank=True)
 
-        objects = models.Manager()
+    objects = models.Manager()
     public = PublicBookmarkManager()
 
-
-class Meta:
+    class Meta:
         verbose_name = 'bookmark'
         verbose_name_plural = 'bookmarks'
         ordering = ['-date_created']
 
-           def __str__(self):
+    def __str__(self):
         return '%s (%s)' % (self.title, self.url)
 
     def save(self, *args, **kwargs):
